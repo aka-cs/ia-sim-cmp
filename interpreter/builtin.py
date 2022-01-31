05e5ca77ccf6459d74f426d8fd313e779acb0653
Daniel Rivero <danielrivgw@gmail.com>
2022-01-31 00:17:49 -0500

    Built-in class hierarchy fixed.

--- interpreter/builtin.py
+++ interpreter/builtin.py
@@ -33,24 +33,25 @@ def get_classes_in_module(module) -> [type]:
 
 def get_classes(module=builtin_code) -> [Class]:
     classes = get_classes_in_module(module)
+    created = set()
     result = []
-    for _class in classes:
-        scope = Scope()
-        c_class = Class(_class[0], None, scope)
-        type_map[_class[1]] = c_class
-    for _class in classes:
-        c_class = type_map[_class[1]]
-        for function in inspect.getmembers(_class[1], inspect.isfunction):
-            function_name = function[0] if function[0] != "__init__" else "init"
-            c_class.scope.declare(function_name, get_builtin_function(function))
-        for var in _class[1].__annotations__:
-            _type = get_type(_class[1].__annotations__[var])
-            c_class.scope.declare(var, _type)
-        super_class = _class[1].__base__ if _class[1].__base__ != object else None
-        c_class.super_class = super_class
-        if super_class:
-            c_class.scope.father = type_map[_class[1].__base__].scope
-        result.append(c_class)
+    while len(created) != len(classes):
+        for _class in classes:
+            if _class[1].__base__ == object or _class[1].__base__ in created:
+                super_class = _class[1].__base__ if _class[1].__base__ != object else None
+                scope = Scope()
+                c_class = Class(_class[0], type_map[super_class] if super_class else None, scope)
+                type_map[_class[1]] = c_class
+                for function in inspect.getmembers(_class[1], inspect.isfunction):
+                    function_name = function[0] if function[0] != "__init__" else "init"
+                    c_class.scope.declare(function_name, get_builtin_function(function))
+                for var in _class[1].__annotations__:
+                    _type = get_type(_class[1].__annotations__[var])
+                    c_class.scope.declare(var, _type)
+                c_class.super_class = super_class
+                
+                result.append(c_class)
+                created.add(_class[1])
     return result
 
 
